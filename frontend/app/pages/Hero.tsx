6d911e5ffa0f05b1e314490b5fcaf9216b426069
jenniferli8263 <75101197+jenniferli8263@users.noreply.github.com>
2024-05-09 21:01:44 -0400

    added assets

---
+++ frontend/app/pages/Hero.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import backgroundImg from "../assets/heroBackground.png";
+import wave from "../assets/wave.svg";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="bg-hero-pattern"></div>
+    </div>
+  );
+};
+
+export default Home;
