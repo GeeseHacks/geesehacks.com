550cb3320f024860a8dac96abdfb6b16692cb1dd
Ri-Hong <riri.hong@gmail.com>
2024-05-12 10:17:12 -0400

    Cleaned file structure for navbar and mlhBadge, added MLH Badge, improved responsiveness of navbar

--- frontend/components/home/navbar.tsx
+++ frontend/components/home/navbar.tsx
@@ -25,8 +25,8 @@ const Navbar: React.FC = () => {
   ));
 
   return (
-    <nav className="w-full bg-transparent text-white fixed top-0 left-0 z-50 lg:pt-4 pt-0">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <nav className="w-screen bg-transparent text-white fixed top-0 left-0 z-50 lg:pt-4 pt-0 px-4 sm:px-6 lg:px-8 xl:px-16">
+      <div className="mx-auto w-full 2xl:w-4/5">
         <div className="flex justify-between h-16">
           <div className="hidden lg:flex">
             <div className="flex-shrink-0 flex items-center">
@@ -47,19 +47,23 @@ const Navbar: React.FC = () => {
               {hamburger}
             </button>
           </div>
-          <div className="hidden lg:flex lg:space-x-4 items-center">
+          {/* the right padding (pr) is used to make space for the MLH Badge. Can remove if MLH badge not used */}
+          <div className="hidden lg:flex lg:space-x-4 items-center pr-36 2xl:pr-0">
             {navLinks}
             <Link
               href="/apply"
-              className="px-6 py-2 bg-transparent border-2 border-white rounded-full text-white text-sm font-medium hover:bg-white hover:text-gray-900 transition-all duration-300 ease-in-out"
+              className="px-6 py-2 bg-transparent border-2 border-white rounded-full text-white text-sm md:text-base lg:text-lg font-medium hover:bg-white hover:text-gray-900 transition-all duration-300 ease-in-out"
             >
               Apply Now
             </Link>
           </div>
         </div>
         {isOpen && (
-          <div className="lg:hidden bg-gray-800 px-2 pt-2 pb-3 space-y-1">
+          <div className="lg:hidden bg-gray-800 px-2 pt-2 pb-3 space-y-1 flex flex-col">
             {navLinks}
+            <Link href="/apply" className={linkStyle}>
+              Apply Now
+            </Link>
           </div>
         )}
       </div>
@@ -69,21 +73,21 @@ const Navbar: React.FC = () => {
 
 export default Navbar;
 
+const linkStyle = "px-3 py-2 rounded-md text-sm md:text-base lg:text-lg font-medium hover:bg-gray-700";
 
-const linkStyle = "px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700";
-
-const hamburger = 
-<svg
-  className="h-6 w-6"
-  xmlns="http://www.w3.org/2000/svg"
-  fill="none"
-  viewBox="0 0 24 24"
-  stroke="currentColor"
->
-  <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth={2}
-    d="M4 6h16M4 12h16M4 18h16"
-  />
-</svg>
+const hamburger = (
+  <svg
+    className="h-6 w-6"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M4 6h16M4 12h16M4 18h16"
+    />
+  </svg>
+);
