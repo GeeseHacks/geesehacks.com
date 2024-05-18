58e5509e0420ef1eda33a0b5ac0b81a5b16047a5
Sarah Kwak <sarahkwak79@gmail.com>
2024-05-18 19:28:46 -0400

    Added about us card

--- frontend/components/home/about.tsx
+++ frontend/components/home/about.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React from "react";
 
-export default function About() {
+const About: React.FC = () => {
   return (
-    <div className="h-screen flex flex-col items-center justify-center bg-gray">
-      <h1 className="text-4xl font-bold text-center text-white">
-        About Us
-      </h1>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-dark-blue relative text-white">
+      <h1 className="text-4xl font-bold text-center mb-10">About Us</h1>
+      <div className="border border-white p-10 md:p-20 md:px-28 rounded-xl shadow-custom text-center max-w-[320px] md:max-w-[600px] lg:max-w-[750px] mx-5 md:mx-10 relative">
+        <span className="large-qoutation left-[0px] top-[-100px]">
+          “
+        </span>
+        <span className="small-qoutation hidden md:inline left-[50px] top-[40px]">
+          “
+        </span>
+        <p className="font-semibold mb-4 text-xl md:text-2xl">
+          Geese Hacks at the University of Waterloo 👏🚀
+        </p>
+        <p className="text-gray-300 text-left text-md md:text-lg">
+          Geese Hacks is an innovative hackathon that encapsulates the spirit of
+          Waterloo’s rigorous tech environment. Organized by students and for
+          students, it's an arena where coding, creativity, and technology
+          collide to transform ideas into reality. This event challenges
+          participants to push the boundaries of technology and develop
+          solutions that address real-world problems.
+        </p>
+        <span className="small-qoutation hidden md:inline right-[50px] bottom-[-10px]">
+          ”
+        </span>
+        <span className="large-qoutation right-[-3px] bottom-[-260px]">
+          ”
+        </span>
+      </div>
     </div>
   );
-}
+};
+
+export default About;
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -12,8 +12,11 @@ const config: Config = {
       backgroundImage: {
         'hero-image': "url('~/assets/heroBackground.png')",
       },
+      
+      boxShadow: {
+        'custom': '0 10px 30px 10px rgba(220, 102, 0, 0.4), 0 5px 10px -2px rgba(220, 102, 0, 0.6)',
+      },
     },
-
   },
   plugins: [],
 };
