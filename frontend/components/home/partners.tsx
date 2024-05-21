a9a083260bfa36aedc740137a86452c244a984ff
Eric <31218485+16BitNarwhal@users.noreply.github.com>
2024-05-21 19:47:53 -0400

    fix mobile responsiveness

--- frontend/components/home/partners.tsx
+++ frontend/components/home/partners.tsx
@@ -10,51 +10,51 @@ export default function Partners() {
         Sponsorship Package
       </button>
       <div className="w-full sm:w-3/4">
-        <div className="max-h-48 grid grid-cols-1 sm:grid-cols-2 gap-4">
-          <div className="flex justify-center items-center bg-white p-4 max-h-48 hover:scale-105 ease-in-out duration-300">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <div className="flex justify-center items-center bg-white p-4 min-h-24 max-h-48 hover:scale-105 ease-in-out duration-300">
             <img
               src="https://uwaterloo.ca/brand/sites/ca.brand/files/waterloo_mathematics_logo_horiz_rgb.png"
               alt="University of Waterloo"
-              className="max-h-full max-w-full object-contain"
+              className="h-auto max-w-full object-contain"
             />
           </div>
-          <div className="flex justify-center items-center bg-white p-4 max-h-48 hover:scale-105 ease-in-out duration-300">
+          <div className="flex justify-center items-center bg-white p-4 min-h-24 max-h-48 hover:scale-105 ease-in-out duration-300">
             <img
               src="https://uwaterloo.ca/brand/sites/ca.brand/files/waterloo_science_logo_horiz_rgb.png"
               alt="University of Waterloo"
-              className="max-h-full max-w-full object-contain"
+              className="h-auto max-w-full object-contain"
             />
           </div>
         </div>
-        <div className="max-h-48 grid grid-cols-1 gap-4 mt-4">
-          <div className="flex justify-center items-center bg-white p-4 max-h-48 hover:scale-105 ease-in-out duration-300">
+        <div className="grid grid-cols-1 gap-4 mt-4">
+          <div className="flex justify-center items-center bg-white p-4 min-h-24 max-h-48 hover:scale-105 ease-in-out duration-300">
             <img
               src="https://starfishmedical.com/assets/UWaterloo-Engineering.jpg"
               alt="University of Waterloo"
-              className="max-h-full max-w-full object-contain"
+              className="h-auto max-w-full object-contain"
             />
           </div>
         </div>
-        <div className="max-h-48 grid grid-cols-1 sm:grid-cols-3 gap-4 mt-4">
-          <div className="flex justify-center items-center bg-white p-4 max-h-48 hover:scale-105 ease-in-out duration-300">
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-4">
+          <div className="flex justify-center items-center bg-white p-4 min-h-24 max-h-48 hover:scale-105 ease-in-out duration-300">
             <img
               src="https://uwaterloo.ca/brand/sites/ca.brand/files/waterloo_health_logo_horiz_rgb.png"
               alt="University of Waterloo"
-              className="max-h-full max-w-full object-contain"
+              className="h-auto max-w-full object-contain"
             />
           </div>
-          <div className="flex justify-center items-center bg-white p-4 max-h-48 hover:scale-105 ease-in-out duration-300">
+          <div className="flex justify-center items-center bg-white p-4 min-h-24 max-h-48 hover:scale-105 ease-in-out duration-300">
             <img
               src="https://uwaterloo.ca/brand/sites/ca.brand/files/waterloo_arts_logo_horiz_rgb.png"
               alt="University of Waterloo"
-              className="max-h-full max-w-full object-contain"
+              className="h-auto max-w-full object-contain"
             />
           </div>
-          <div className="flex justify-center items-center bg-white p-4 max-h-48 hover:scale-105 ease-in-out duration-300">
+          <div className="flex justify-center items-center bg-white p-4 min-h-24 max-h-48 hover:scale-105 ease-in-out duration-300">
             <img
               src="https://uwaterloo.ca/brand/sites/ca.brand/files/waterloo_environment_logo_horiz_rgb.png"
               alt="University of Waterloo"
-              className="max-h-full max-w-full object-contain"
+              className="h-auto max-w-full object-contain"
             />
           </div>
         </div>
