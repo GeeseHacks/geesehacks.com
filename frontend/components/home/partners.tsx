067ffae6de3d6280029fb329ea9b3921182d51cf
Eric <31218485+16BitNarwhal@users.noreply.github.com>
2024-05-21 23:25:11 -0400

    add images and fix sizing

--- frontend/components/home/partners.tsx
+++ frontend/components/home/partners.tsx
@@ -11,14 +11,14 @@ export default function Partners() {
       </button>
       <div className="w-full sm:w-3/4">
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          <div className="flex justify-center items-center bg-white p-4 min-h-24 max-h-48 hover:scale-105 ease-in-out duration-300">
+          <div className="flex justify-center items-center bg-white p-4 min-h-[6rem] hover:scale-105 ease-in-out duration-300">
             <img
               src="/images/waterloo_mathematics_logo_horiz_rgb.png"
               alt="University of Waterloo"
               className="h-auto max-w-full object-contain"
             />
           </div>
-          <div className="flex justify-center items-center bg-white p-4 min-h-24 max-h-48 hover:scale-105 ease-in-out duration-300">
+          <div className="flex justify-center items-center bg-white p-4 min-h-[6rem] hover:scale-105 ease-in-out duration-300">
             <img
               src="/images/waterloo_science_logo_horiz_rgb.png"
               alt="University of Waterloo"
@@ -27,7 +27,7 @@ export default function Partners() {
           </div>
         </div>
         <div className="grid grid-cols-1 gap-4 mt-4">
-          <div className="flex justify-center items-center bg-white p-4 min-h-24 max-h-48 hover:scale-105 ease-in-out duration-300">
+          <div className="flex justify-center items-center bg-white p-4 min-h-[6rem] hover:scale-105 ease-in-out duration-300">
             <img
               src="/images/UWaterloo-Engineering.jpg"
               alt="University of Waterloo"
@@ -36,21 +36,21 @@ export default function Partners() {
           </div>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-4">
-          <div className="flex justify-center items-center bg-white p-4 min-h-24 max-h-48 hover:scale-105 ease-in-out duration-300">
+          <div className="flex justify-center items-center bg-white p-4 min-h-[6rem] hover:scale-105 ease-in-out duration-300">
             <img
               src="/images/waterloo_health_logo_horiz_rgb.png"
               alt="University of Waterloo"
               className="h-auto max-w-full object-contain"
             />
           </div>
-          <div className="flex justify-center items-center bg-white p-4 min-h-24 max-h-48 hover:scale-105 ease-in-out duration-300">
+          <div className="flex justify-center items-center bg-white p-4 min-h-[6rem] hover:scale-105 ease-in-out duration-300">
             <img
               src="/images/waterloo_arts_logo_horiz_rgb.png"
               alt="University of Waterloo"
               className="h-auto max-w-full object-contain"
             />
           </div>
-          <div className="flex justify-center items-center bg-white p-4 min-h-24 max-h-48 hover:scale-105 ease-in-out duration-300">
+          <div className="flex justify-center items-center bg-white p-4 min-h-[6rem] hover:scale-105 ease-in-out duration-300">
             <img
               src="/images/waterloo_environment_logo_horiz_rgb.png"
               alt="University of Waterloo"
