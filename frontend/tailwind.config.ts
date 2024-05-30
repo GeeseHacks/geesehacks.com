af8b9da539d8d2cf2525031afacf6981fd406b22
Ri-Hong <riri.hong@gmail.com>
2024-05-30 19:42:53 -0400

    Added text glow in hero section

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from "tailwindcss";
+import { Config } from "tailwindcss";
+import { PluginAPI } from "tailwindcss/types/config";
 
 const config: Config = {
   content: [
@@ -13,12 +14,21 @@ const config: Config = {
         'hero-image': "url('~/assets/heroBackground.png')",
         'wave-image': "url('~/assets/wave.png')",
       },
-
       boxShadow: {
-        'purple-shadow': '0 0 20px 5px rgba(158, 99, 234, 0.6)'
-      },
+        'purple-shadow': '0 0 20px 5px rgba(158, 99, 234, 0.6)', // For About Us box
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    function (api: PluginAPI) {
+      const newUtilities = {
+        '.text-shadow-white-glow': {
+          textShadow: '0px 0px 25px rgba(255,255,255,0.65)',
+        },
+      };
+      api.addUtilities(newUtilities, { respectPrefix: true, respectImportant: true });
+    },
+  ],
 };
+
 export default config;
