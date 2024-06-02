a77cbb232cbf58411e5e6812d169a8297ac1101b
Sarah Kwak <sarahkwak79@gmail.com>
2024-06-02 15:04:17 -0400

    added purple blurred circle in partners

--- frontend/components/home/partners.tsx
+++ frontend/components/home/partners.tsx
@@ -2,20 +2,21 @@ import React from "react";
 
 export default function Partners() {
   return (
-    <div className="p-8 sm:p-16 flex flex-col items-center justify-center">
+    <div className="p-8 sm:p-16 flex flex-col items-center justify-center relative">
       <h1 className="text-2xl sm:text-4xl font-bold text-center text-white mb-4 sm:mb-8">
         Our Partners
       </h1>
-      <div className="w-full sm:w-3/4">
+      <div className="absolute w-72 h-72 md:w-80 md:h-80 lg:w-96 lg:h-96 bg-purple-600 rounded-full blur-2xl opacity-40" style={{ top: '30%' }}></div>
+      <div className="w-full sm:w-3/4 z-10">
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          <div className="flex justify-center items-center bg-purple-300 bg-opacity-15 border border-purple-600 rounded-md p-4 min-h-[6rem] hover:scale-105 ease-in-out duration-300">
+          <div className="flex justify-center items-center bg-purple-300 bg-opacity-15 border border-purple-300 rounded-md p-4 min-h-[6rem] hover:scale-105 ease-in-out duration-300">
             <img
               src="/images/waterloo_mathematics_logo_horiz_rgb.png"
               alt="University of Waterloo"
               className="h-auto max-w-full object-contain"
             />
           </div>
-          <div className="flex justify-center items-center bg-purple-300 bg-opacity-15 border border-purple-600 rounded-md p-4 min-h-[6rem] hover:scale-105 ease-in-out duration-300">
+          <div className="flex justify-center items-center bg-purple-300 bg-opacity-15 border border-purple-300 rounded-md p-4 min-h-[6rem] hover:scale-105 ease-in-out duration-300">
             <img
               src="/images/waterloo_science_logo_horiz_rgb.png"
               alt="University of Waterloo"
@@ -24,7 +25,7 @@ export default function Partners() {
           </div>
         </div>
         <div className="grid grid-cols-1 gap-4 mt-4">
-          <div className="flex justify-center items-center bg-purple-300 bg-opacity-15 border border-purple-600 rounded-md p-4 min-h-[6rem] hover:scale-105 ease-in-out duration-300">
+          <div className="flex justify-center items-center bg-purple-300 bg-opacity-15 border border-purple-300 rounded-md p-4 min-h-[6rem] hover:scale-105 ease-in-out duration-300">
             <img
               src="/images/UWaterloo-Engineering.jpg"
               alt="University of Waterloo"
@@ -33,21 +34,21 @@ export default function Partners() {
           </div>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-4">
-          <div className="flex justify-center items-center bg-purple-300 bg-opacity-15 border border-purple-600 rounded-md p-4 min-h-[6rem] hover:scale-105 ease-in-out duration-300">
+          <div className="flex justify-center items-center bg-purple-300 bg-opacity-15 border border-purple-300 rounded-md p-4 min-h-[6rem] hover:scale-105 ease-in-out duration-300">
             <img
               src="/images/waterloo_health_logo_horiz_rgb.png"
               alt="University of Waterloo"
               className="h-auto max-w-full object-contain"
             />
           </div>
-          <div className="flex justify-center items-center bg-purple-300 bg-opacity-15 border border-purple-600 rounded-md p-4 min-h-[6rem] hover:scale-105 ease-in-out duration-300">
+          <div className="flex justify-center items-center bg-purple-300 bg-opacity-15 border border-purple-300 rounded-md p-4 min-h-[6rem] hover:scale-105 ease-in-out duration-300">
             <img
               src="/images/waterloo_arts_logo_horiz_rgb.png"
               alt="University of Waterloo"
               className="h-auto max-w-full object-contain"
             />
           </div>
-          <div className="flex justify-center items-center bg-purple-300 bg-opacity-15 border border-purple-600 rounded-md p-4 min-h-[6rem] hover:scale-105 ease-in-out duration-300">
+          <div className="flex justify-center items-center bg-purple-300 bg-opacity-15 border border-purple-300 rounded-md p-4 min-h-[6rem] hover:scale-105 ease-in-out duration-300">
             <img
               src="/images/waterloo_environment_logo_horiz_rgb.png"
               alt="University of Waterloo"
