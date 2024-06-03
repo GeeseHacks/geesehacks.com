8024e2291029b3c5aa3d43b7d03857ba35a7b6ee
Brianna <132308375+briannale935@users.noreply.github.com>
2024-06-02 23:25:31 -0400

    stats box

--- frontend/components/home/stats.tsx
+++ frontend/components/home/stats.tsx
@@ -18,38 +18,46 @@ export default function Stats() {
           />
         </div>
       </div>
-      <div className="flex flex-wrap justify-center items-center mt-56 gap-x-10">
-        <div className="flex flex-col items-center rounded-3xl">
-          <h1 className="inline-block pr-20 pl-20 pb-3 sm:text-2xl md:text-3xl lg:text-5xl xl:text-5xl font-bold text-white">
+      <div className="flex flex-wrap justify-center items-center mt-56 gap-4">
+      <div style={{ width: 'calc(20% - 4px)' }}>
+        <div className="flex flex-col items-center bg-opacity-15 border border-purple-300 rounded-2xl p-4">
+          <h1 className="inline-block sm:text-2xl md:text-3xl lg:text-5xl xl:text-5xl font-bold text-white">
             1000+
           </h1>
-          <div className="text-sm sm:text-base md:text-base lg:text-m xl:text-l font-normal text-gray-500 pb-10">
+          <div className="text-sm sm:text-base md:text-base lg:text-m xl:text-l font-normal text-gray-500">
             Applicants
           </div>
         </div>
-        <div className="flex flex-col items-center rounded-3xl">
-          <h1 className="inline-block pr-20 pl-20 pb-3 sm:text-2xl md:text-3xl lg:text-5xl xl:text-5xl font-bold text-white">
+      </div>
+      <div style={{ width: 'calc(20% - 4px)' }}>
+        <div className="flex flex-col items-center bg-gray-900 bg-opacity-15 border border-purple-300 rounded-2xl p-4">
+          <h1 className="inline-block sm:text-2xl md:text-3xl lg:text-5xl xl:text-5xl font-bold text-white">
             25+
           </h1>
-          <div className="text-sm sm:text-base md:text-base lg:text-m xl:text-l font-normal text-gray-500 pb-10">
+          <div className="text-sm sm:text-base md:text-base lg:text-m xl:text-l font-normal text-gray-500">
             Events
           </div>
         </div>
-        <div className="flex flex-col items-center">
-          <h1 className="inline-block pr-20 pl-20 pb-3 sm:text-2xl md:text-3xl lg:text-5xl xl:text-5xl font-bold text-white">
+      </div>
+      <div style={{ width: 'calc(20% - 4px)' }}>
+        <div className="flex flex-col items-center bg-opacity-15 border border-purple-300 rounded-2xl p-4">
+          <h1 className="inline-block sm:text-2xl md:text-3xl lg:text-5xl xl:text-5xl font-bold text-white">
             50+
           </h1>
-          <div className="text-sm sm:text-base md:text-base lg:text-m xl:text-l font-normal text-gray-500 pb-10">
+          <div className="text-sm sm:text-base md:text-base lg:text-m xl:text-l font-normal text-gray-500">
             Organizers
           </div>
         </div>
-        <div className="flex flex-col items-center">
-          <h1 className="inline-block pr-20 pl-20 pb-3 sm:text-2xl md:text-3xl lg:text-5xl xl:text-5xl font-bold text-white">
+      </div>
+      <div style={{ width: 'calc(20% - 4px)' }}>
+        <div className="flex flex-col items-center bg-opacity-15 border border-purple-300 rounded-2xl p-4">
+          <h1 className="inline-block sm:text-2xl md:text-3xl lg:text-5xl xl:text-5xl font-bold text-white">
             40+
           </h1>
-          <div className="text-sm sm:text-base md:text-base lg:text-m xl:text-l font-normal text-gray-500 pb-10">
+          <div className="text-sm sm:text-base md:text-base lg:text-m xl:text-l font-normal text-gray-500">
             Countries
           </div>
+      </div>
         </div>
       </div>
     </div>
