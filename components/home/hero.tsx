f51daab5db0f689bee29a9f3638c0297c7852d2c
Sarah Kwak <sarahkwak79@gmail.com>
2024-07-22 14:59:50 -0400

    refactoring and image change

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -4,8 +4,9 @@ import React, { Suspense, lazy } from "react";
 import HeroButton from "./utils/heroButton";
 import { FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import { motion } from "framer-motion";
+import Image from 'next/image';
+import backgroundImage from "@/public/images/heroBackground.webp";
 import Head from "next/head";
-import backgroundImage from "@/public/images/heroBackground.png";
 
 // Lazy load AnimatedTerminal
 const AnimatedTerminal = lazy(() => import("./utils/terminal"));
@@ -34,16 +35,25 @@ const itemVariants = {
 const Hero = () => {
   return (
     <>
-      <Head>
+    <Head>
         <title>GeeseHacks - Hero Section</title>
         <meta name="description" content="Join us at GeeseHacks, happening from January 10-12 in Waterloo, ON. Stay tuned for hacker applications!" />
       </Head>
-      <div className="bg-cover w-full h-screen flex items-center justify-center md:justify-evenly bg-bottom" style={{ backgroundImage: `url(${backgroundImage.src})` }} aria-label="Hero section">
-        <div className="flex-col space-y-8 mb-3 mx-3">
+      <div className="bg-cover w-full h-screen flex items-center justify-center md:justify-evenly bg-bottom" aria-label="Hero section">
+        <div className="absolute inset-0">
+          <Image
+            src={backgroundImage}
+            alt="Hero Background"
+            fill
+            priority
+            placeholder="blur"
+          />
+        </div>
+        <div className="flex-col space-y-8 mb-3 mx-3 relative z-10">
           <motion.div initial="hidden" animate="visible" variants={containerVariants}>
             <motion.h1 variants={itemVariants} className="text-white text-2xl text-center mx-2 md:text-left lg:text-3xl text-shadow-white-glow">
               January 10-12 · Waterloo, ON
-            </motion.h1>
+            </motion.h1>  
 
             <motion.h1 variants={itemVariants} className="text-white text-center text-5xl my-6 sm:text-6xl lg:text-7xl xl:text-8xl font-bold text-shadow-white-glow">
               GeeseHacks
