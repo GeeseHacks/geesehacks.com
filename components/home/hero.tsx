778eea4cc896355d1eeb72e59c2fd101ad4999b2
Ri-Hong <riri.hong@gmail.com>
2024-12-04 13:01:32 -0500

    Enabled Apply Now buttons and updated routes

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -12,7 +12,7 @@ import Head from "next/head";
 const AnimatedTerminal = lazy(() => import("./utils/terminal"));
 
 // SET TO TRUE WHEN APPLICATION OPENS
-const applicationOpen = false;
+const applicationOpen = true;
 
 const containerVariants = {
   hidden: { opacity: 0, y: -30 },
@@ -52,7 +52,7 @@ const Hero = () => {
 
             <motion.div variants={itemVariants} className="space-y-6">
               <h1 className="text-white mx-2 text-center font-semilight text-lg lg:text-xl lg:text-left text-shadow-white-glow">
-                Hacker applications open soon. Stay tuned!
+                Hacker applications open!
               </h1>
             </motion.div>
           </motion.div>
@@ -72,7 +72,7 @@ const Hero = () => {
 
           {applicationOpen && (
             <div className="flex space-x-4 justify-center md:justify-start">
-              <HeroButton href="/apply">Apply Now</HeroButton>
+              <HeroButton>Apply Now</HeroButton>
             </div>
           )}
         </div>
